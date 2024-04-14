ee0c9cd4c93c638d7537b4ee3eae9fdbdf1b50c0
mexsov <151562135+mexsov@users.noreply.github.com>
2024-04-14 11:53:06 +0300

    restored registration form, added links to:homepage,login. created registration form css, updated dependencies

--- SantaMonicaTechPioneersClient/src/App.jsx
+++ SantaMonicaTechPioneersClient/src/App.jsx
@@ -1,24 +1,64 @@
 import { useState } from 'react'
+import { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import RegistrationForm from "./components/RegisterForm"
 import {Home} from './components/homepage'
+import LoginForm from "./components/LoginForm";
+import UserDashboard from "./components/UserDashboard";
+import AdminDashboard from "./components/AdminDashboard";
+import PrivateRoute from "./routes/privateRoutes";
+import { AuthContext } from "./utils/AuthContext";
 
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./App.css";
 import './App.css'
 
 function App() {
   
+  const { user: authUser, logoutUser } = useContext(AuthContext);
 
   return (
     <>
+     <Header />
     <Router>
+    <nav>
+          <ul>
+            {!authUser ? (
+              <>
+
+              </>
+            ) : ( 
+              <li>
+                 <button onClick={logoutUser}>Logout</button>
+                 </li>)}
+                 </ul>
+        </nav>
       <Routes>
+      <Route path="/login" element={<LoginForm />} />
         <Route path='/Signup' element={<RegistrationForm />} />
-        <Route path='/Home' element={<Home />} />
-        {/* <Route path='*' element={<Home/>}/> */}
+        <Route path='/' element={<Home />} />
+        <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute roles={["user"]}>
+                <UserDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/admin-dashboard"
+            element={
+              <PrivateRoute roles={["admin"]}>
+                <AdminDashboard />
+              </PrivateRoute>
+            }
+          />
      
       </Routes>
     </Router>
-      
+    <Footer />
     </>
   )
 }
--- SantaMonicaTechPioneersClient/src/components/Header.jsx
+++ SantaMonicaTechPioneersClient/src/components/Header.jsx
@@ -7,12 +7,12 @@ const Header = () => {
     <header>
       <Navbar bg="primary" variant='primary' data-bs-theme="dark" collapseOnSelect>
         <Container className="header">
-          <Navbar.Brand href="#home"><h1>SantaMonicaTechPioneersClient</h1></Navbar.Brand>
+          <Navbar.Brand href="/"><h1>SantaMonicaTechPioneersClient</h1></Navbar.Brand>
           <Nav className="ml-auto">
             <Nav.Link href="./Login"> <i className='fas fa-user'></i>Log in</Nav.Link>
             <Nav.Link href="#features"><i className='fas fa-address-book'></i>Contact</Nav.Link>
             <Nav.Link href="#pricing"><i className='fas fa-dollar-sign'></i> Pricing</Nav.Link>
-            <Nav.Link href="#GetStarted"><i className='fas fa-play'></i> Get started</Nav.Link>
+            <Nav.Link href="/signup"><i className='fas fa-play'></i> Get started</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
