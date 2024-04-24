f9a49a53f58636688148efc4fcf86f6d433cf90b
Zygis911 <zygis.cer@gmail.com>
2024-04-24 17:54:41 +0300

    renew sql

--- server/db/DatabaseDesign.sql
+++ server/db/DatabaseDesign.sql
@@ -36,7 +36,7 @@ CREATE TABLE tasks(
     CONSTRAINT fk_projects FOREIGN KEY(project_id) REFERENCES projects(id)
 );
 
-
+-- UPDATE_23-04-2024_________________________________________________________________________________
 
 ALTER TYPE ROLE ADD VALUE 'super_user';
 
