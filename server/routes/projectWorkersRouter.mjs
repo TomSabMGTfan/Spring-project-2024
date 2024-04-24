40c01fe006c73bcab028996de8261ad8ba78f5ca
Daniusvl <danielius.vl@gmail.com>
2024-04-25 01:11:05 +0300

    Added validation to p worker update & delete endpoints

--- server/routes/projectWorkersRouter.mjs
+++ server/routes/projectWorkersRouter.mjs
@@ -6,12 +6,13 @@ import AuthMiddleware from '../middleware/authMiddleware.mjs';
 
 import projectWorkersController from '../controllers/projectWorkersController.mjs';
 
+import { updatePWorkerValidationSchema, deletePWorkerValidationSchema } from '../validators/projectWorkersValidator.mjs';
 
 const router = express.Router();
 
-router.get('/byuser/:user_id', [AuthMiddleware], projectWorkersController.getPWorkersByUserId);
-router.update("/", AuthMiddleware, projectWorkersController.updatePWorker);
-router.delete("/", AuthMiddleware, projectWorkersController.deletePWorker);
+router.get('/byuser/:user_id', AuthMiddleware, projectWorkersController.getPWorkersByUserId);
+router.update("/", [AuthMiddleware, updatePWorkerValidationSchema], projectWorkersController.updatePWorker);
+router.delete("/", [AuthMiddleware, deletePWorkerValidationSchema], projectWorkersController.deletePWorker);
 
 
 export default router;
---
+++ server/validators/projectWorkersValidator.mjs
@@ -0,0 +1,41 @@
+import { checkSchema } from 'express-validator';
+import { USER, ADMIN } from '../cfg/Roles.mjs';
+
+export const updatePWorkerValidationSchema = checkSchema({
+    role:{
+        custom:{
+			options: async (value) => {
+				if(value != USER && value != ADMIN){
+                    throw new Error("Specified role does not exist in the system");
+                }
+			},
+		}
+    },
+    user_id: {
+        isInt:{
+            options: {min: 1},
+            errorMessage: "user_id must be a valid positive integer"
+        }
+    },
+    project_id: {
+        isInt:{
+            options: {min: 1},
+            errorMessage: "user_id must be a valid positive integer"
+        }
+    }
+});
+
+export const deletePWorkerValidationSchema = checkSchema({
+    user_id: {
+        isInt:{
+            options: {min: 1},
+            errorMessage: "user_id must be a valid positive integer"
+        }
+    },
+    project_id: {
+        isInt:{
+            options: {min: 1},
+            errorMessage: "user_id must be a valid positive integer"
+        }
+    }
+});
