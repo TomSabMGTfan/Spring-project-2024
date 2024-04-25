89ea1da86eea97078adf6e272970fc89a90e8d9a
Zygis911 <zygis.cer@gmail.com>
2024-04-25 21:46:08 +0300

    changes to project Model

--- server/models/projectsModel.mjs
+++ server/models/projectsModel.mjs
@@ -1,7 +1,5 @@
 import { pool } from "../db/postgresConnection.mjs";
 
-
-
 const projectsModel = {
   createProject: async (name, description) => {
     const status = "in progress";
@@ -12,9 +10,29 @@ const projectsModel = {
     return result.rows[0];
   },
 
-  deleteProject: async(id) => {
-    await pool.query('DELETE FROM projects WHERE id = $1', [id])
-  }
+  deleteProject: async (id) => {
+    await pool.query("DELETE FROM projects WHERE id = $1 AND user_id = $2", [
+      id,
+      user_id,
+    ]);
+  },
+
+  updateProject: async (id, updatedProject) => {
+    const { name, description, status } = updatedProject;
+    const result = await pool.query(
+      "UPDATE projects SET name = 1$, description = 2$, status = 3$ WHERE id = 4$ RETURNING *",
+      [name, description, status, id]
+    );
+    return result.rows[0];
+  },
+
+  getUserProjects: async (user_id) => {
+    const result = await pool.query(
+      "SELECT projects.* FROM users JOIN projects ON user_id = projects_id WHERE user_id = 1$",
+      [user_id]
+    );
+    return result.rows[0];
+  },
 };
 
 export default projectsModel;
