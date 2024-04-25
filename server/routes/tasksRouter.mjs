2372738481e618c6f872e243d6680a359e4d6e21
Daniusvl <danielius.vl@gmail.com>
2024-04-25 03:53:11 +0300

    Added task validation

--- server/routes/tasksRouter.mjs
+++ server/routes/tasksRouter.mjs
@@ -4,16 +4,16 @@ import dotenv from 'dotenv/config';
 import AuthMiddleware from '../middleware/authMiddleware.mjs';
 
 import tasksController from '../controllers/tasksController.mjs';
-
+import { createTaskValidationSchema, updateTaskValidationSchema } from '../validators/taskValidator.mjs';
 
 
 const router = express.Router();
 
 
 
-router.post('/', [AuthMiddleware, null], tasksController.createTask);
+router.post('/', [AuthMiddleware, createTaskValidationSchema], tasksController.createTask);
 router.get("/:project_id", tasksController.getTasksByProjectId);
-router.put("/", [AuthMiddleware, null], tasksController.updateTask);
+router.put("/", [AuthMiddleware, updateTaskValidationSchema], tasksController.updateTask);
 router.delete("/:id", AuthMiddleware, tasksController.deleteTask);
 
 
---
+++ server/validators/taskValidator.mjs
@@ -0,0 +1,102 @@
+import { checkSchema } from 'express-validator';
+
+export const createTaskValidationSchema = checkSchema({
+    name: {
+        notEmpty:{
+            errorMessage: "Name cannot be empty"
+        },
+        isString:{
+            errorMessage: "Name must be a valid String"
+        },
+        isLength:{
+            options: {min:2, max:50},
+            errorMessage: "Name must be at least 2 characters with a max of 50 characters"
+        }
+    },
+    description:{
+        notEmpty:{
+            errorMessage: "Description cannot be empty"
+        },
+        isString:{
+            errorMessage: "Description must be a valid String"
+        },
+        isLength:{
+            options: {min:10, max:5000},
+            errorMessage: "Description must be at least 10 characters with a max of 5000 characters"
+        }
+    },
+    project_id: {
+        isInt:{
+            options: {min: 1},
+            errorMessage: "project_id must be a valid positive integer"
+        }
+    },
+    planned_end_date:{
+        isDate:{
+            errorMessage: "Planned end date mus be a valid Date"
+        },
+        custom:{
+            options: (value) => {
+                if(value < Date.now()){
+                    throw new Error("Planned end date must be greater than current date");
+                } 
+            }
+        }
+    }
+});
+
+export const updateTaskValidationSchema = checkSchema({
+    id: {
+        isInt:{
+            options: {min: 1},
+            errorMessage: "project_id must be a valid positive integer"
+        }
+    },
+    name: {
+        notEmpty:{
+            errorMessage: "Name cannot be empty"
+        },
+        isString:{
+            errorMessage: "Name must be a valid String"
+        },
+        isLength:{
+            options: {min:2, max:50},
+            errorMessage: "Name must be at least 2 characters with a max of 50 characters"
+        }
+    },
+    description:{
+        notEmpty:{
+            errorMessage: "Description cannot be empty"
+        },
+        isString:{
+            errorMessage: "Description must be a valid String"
+        },
+        isLength:{
+            options: {min:10, max:5000},
+            errorMessage: "Description must be at least 10 characters with a max of 5000 characters"
+        }
+    },
+    project_id: {
+        isInt:{
+            options: {min: 1},
+            errorMessage: "project_id must be a valid positive integer"
+        }
+    },
+    created_on:{
+        isDate:{
+            errorMessage: "Created on mus be a valid Date"
+        }
+    },
+    planned_end_date:{
+        isDate:{
+            errorMessage: "Planned end date mus be a valid Date"
+        },
+        custom:{
+            options: (value) => {
+                if(value < Date.now()){
+                    throw new Error("Planned end date must be greater than current date");
+                } 
+            }
+        }
+    }
+});
