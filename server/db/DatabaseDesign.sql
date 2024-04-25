5df4319f6bbace34ae0006c66159b42c5068c29a
Daniusvl <danielius.vl@gmail.com>
2024-04-25 04:26:40 +0300

    Added explanatory comments

--- server/db/DatabaseDesign.sql
+++ server/db/DatabaseDesign.sql
@@ -6,23 +6,30 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL UNIQUE, 
     password VARCHAR(255) NOT NULL, 
     email VARCHAR(255) NOT NULL UNIQUE, 
+    -- role no longer exists
     role ROLE NOT NULL DEFAULT 'user',
     registered_on DATE NOT NULL 
 );
 
 CREATE TYPE STATUS AS ENUM ('in progress', 'done');
+-- STATUS no longer exists there is now:
+-- PROJECT_STATUS ('ongoing', 'done')
+-- TASK_STATUS ('to do', 'in progress', 'done')
 
 CREATE TABLE projects (
     id SERIAL PRIMARY KEY, 
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     status STATUS NOT NULL DEFAULT 'in progress'
+--  status replaced with: 
+--  status PROJECT_STATUS NOT NULL DEFAULT 'ongoing'
 );
 
 CREATE TABLE project_workers(
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     project_id INT NOT NULL,
+--  role ROLE NOT NULL DEFAULT 'user';
     CONSTRAINT fk_users FOREIGN KEY(user_id) REFERENCES users(id),
     CONSTRAINT fk_project FOREIGN KEY(project_id) REFERENCES projects(id)
 );
@@ -31,8 +38,14 @@ CREATE TABLE tasks(
     id SERIAL PRIMARY KEY, 
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
+--  created_on DATE NOT NULL,
+--  planned_end_date DATE NOT NULL,
     status STATUS NOT NULL DEFAULT 'in progress',
+--  status replaced with:
+--  status TASK_STATUS NOT NULL DEFAULT 'to do',
     project_id INT NOT NULL,
+--  worker_id INT
+--  CONSTRAINT fk_users FOREIGN KEY(worker_id) REFERENCES users(id);
     CONSTRAINT fk_projects FOREIGN KEY(project_id) REFERENCES projects(id)
 );
 
@@ -75,4 +88,6 @@ ALTER TYPE ROLE ADD VALUE 'owner';
 
 ALTER TABLE tasks
 ADD created_on DATE NOT NULL,
-ADD planned_end_date DATE NOT NULL;
+ADD planned_end_date DATE NOT NULL
+DROP status,
+ADD status TASK_STATUS NOT NULL DEFAULT 'to do';
