9aaeaa42a57b4fe8144de61de54491ef6ffa370a
Daniusvl <danielius.vl@gmail.com>
2024-04-25 21:40:45 +0300

    Added new endpoint & fixed bugs

--- server/controllers/projectWorkersController.mjs
+++ server/controllers/projectWorkersController.mjs
@@ -1,3 +1,5 @@
+import { validationResult } from "express-validator";
+
 import project_workersModel from "../models/project_workersModel.mjs";
 
 import { ADMIN, OWNER, USER } from "../cfg/Roles.mjs";
@@ -6,8 +8,21 @@ const projectWorkersController = {
     getPWorkersByUserId: async (req, res) => {
         try{
             const id = req.params.user_id;
-
+            console.log(id);
             const result = await project_workersModel.getProjectWorkersByUserId(id);
+
+            return res.status(200).json(result);
+        }
+        catch(error){
+            console.log(error);
+            return res.status(500).json({message: "Failed to get a project worker, an error has occured"});
+        }
+    },
+    getPWorkerByUserAndProjectId: async(req, res)=>{
+        try{
+            const {user_id, project_id} = req.params;
+
+            const result = await project_workersModel.getProjectWorker(user_id, project_id);
 
             return res.status(200).json(result);
         }
@@ -29,6 +44,48 @@ const projectWorkersController = {
             return res.status(500).json({message: "Failed to get a project worker, an error has occured"});
         }
     },
+
+    createPWorker:async(req, res)=>{
+         try{
+            
+            const {user_id, project_id} = req.body;
+
+            if(!req.user){
+                return res.status(401).json("Unauthorized access");
+            }
+
+            const currWorker = await project_workersModel.getProjectWorker(req.user.id, project_id);
+
+            if(!currWorker){
+                return res.status(401).json("You dont have privileges to perform this action");
+            }
+
+            const {role} = currWorker;
+
+            if(role == USER){
+                return res.status(401).json("You dont have privileges to perform this action");
+            }
+
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const pWorker = {
+                role: USER, user_id, project_id
+            };
+
+            const result = await project_workersModel.createProjectWorker(pWorker);
+
+            return res.status(200).json(result);
+
+        }
+        catch(error){
+            console.log(error);
+            return res.status(500).json({message: "Failed to create a project worker, an error has occured"});
+        }
+    },
+
     updatePWorker: async (req,res) => {
         try{
             
@@ -38,14 +95,25 @@ const projectWorkersController = {
                 return res.status(401).json("Unauthorized access");
             }
 
-            const {role:currentUserRole} = await project_workersModel.getProjectWorker(req.user.id, project_id);
-            const {role:otherUserRole} = await project_workersModel.getProjectWorker(user_id, project_id);
+            const currentUserRole = await project_workersModel.getProjectWorker(req.user.id, project_id);
+            const otherUserRole = await project_workersModel.getProjectWorker(user_id, project_id);
 
-            if(otherUserRole === OWNER){
+            if(!currentUserRole){
                 return res.status(401).json("You dont have privileges to perform this action");
             }
 
-            if(currentUserRole == USER){
+            if(!otherUserRole){
+                return res.status(400).json("This user is a project worker");
+            }
+
+            const {role: currentRole} = currentUserRole;
+            const {role: otherRole} = otherUserRole;
+
+            if(otherRole === OWNER){
+                return res.status(401).json("You dont have privileges to perform this action");
+            }
+
+            if(currentRole == USER){
                 return res.status(401).json("You dont have privileges to perform this action");
             }
 
@@ -77,14 +145,25 @@ const projectWorkersController = {
                 return res.status(401).json("Unauthorized access");
             }
 
-            const {role:currentUserRole} = await project_workersModel.getProjectWorker(req.user.id, project_id);
-            const {role:otherUserRole} = await project_workersModel.getProjectWorker(user_id, project_id);
+            const currentUserRole = await project_workersModel.getProjectWorker(req.user.id, project_id);
+            const otherUserRole = await project_workersModel.getProjectWorker(user_id, project_id);
 
-            if(otherUserRole === OWNER){
+            if(!currentUserRole){
                 return res.status(401).json("You dont have privileges to perform this action");
             }
 
-            if(currentUserRole == USER){
+            if(!otherUserRole){
+                return res.status(400).json("This user is not a project worker");
+            }
+
+            const {role: currentRole} = currentUserRole;
+            const {role: otherRole} = otherUserRole;
+
+            if(otherRole === OWNER){
+                return res.status(401).json("You dont have privileges to perform this action");
+            }
+
+            if(currentRole == USER){
                 return res.status(401).json("You dont have privileges to perform this action");
             }
             
@@ -95,7 +174,7 @@ const projectWorkersController = {
 
             await project_workersModel.deleteProjectWorker(user_id, project_id);
 
-            return res.status(200);
+            return res.status(200).json();
 
         }
         catch(error){
--- server/routes/projectWorkersRouter.mjs
+++ server/routes/projectWorkersRouter.mjs
@@ -6,12 +6,15 @@ import AuthMiddleware from '../middleware/authMiddleware.mjs';
 
 import projectWorkersController from '../controllers/projectWorkersController.mjs';
 
-import { updatePWorkerValidationSchema, deletePWorkerValidationSchema } from '../validators/projectWorkerValidator.mjs';
+import { updatePWorkerValidationSchema, deletePWorkerValidationSchema,
+         createPWorkerValidationSchema } from '../validators/projectWorkerValidator.mjs';
 
 const router = express.Router();
 
 router.get('/user/:user_id', AuthMiddleware, projectWorkersController.getPWorkersByUserId);
 router.get('/project/:project_id', AuthMiddleware, projectWorkersController.getPWorkersByProjectId);
+router.get("/:user_id/:project_id", AuthMiddleware, projectWorkersController.getPWorkerByUserAndProjectId);
+router.post("/", [AuthMiddleware, createPWorkerValidationSchema], projectWorkersController.createPWorker);
 router.put("/", [AuthMiddleware, updatePWorkerValidationSchema], projectWorkersController.updatePWorker);
 router.delete("/", [AuthMiddleware, deletePWorkerValidationSchema], projectWorkersController.deletePWorker);
 
