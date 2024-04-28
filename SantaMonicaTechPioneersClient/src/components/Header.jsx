d6a5ba3b2deb66e7939aa2afd67a101eb8234a60
Parizo <smkvilnius@gmail.com>
2024-04-28 22:16:39 +0300

    Breathing santa monica

--- SantaMonicaTechPioneersClient/src/components/Header.jsx
+++ SantaMonicaTechPioneersClient/src/components/Header.jsx
@@ -17,7 +17,54 @@ function Navbar() {
 	};
 
 	return (
-		<header>
+		<>
+<header>
+<h5 className=""> {authUser ?`Logged in as ${authUser.username}` : ''}</h5>
+	<nav ref={navRef}>
+	{!authUser ? (
+		<>
+		<div className="logo"><a href="/"><p className="logo-collor">Santa Monica Tech Pioneers</p></a></div>
+		<div>
+	</div>
+		{/* <ul>
+			<li><a href=""><i class="fas fa-home"></i>Home</a></li>
+			<li><a href=""><i class="fas fa-info-circle"></i>About</a></li>
+			<li><a href=""><i class="fas fa-cogs"></i>Services</a></li>
+			<li><a href=""><i class="fas fa-log-in"></i>Contacts</a></li>
+		</ul> */}
+		<ul>
+		<li><a href="/login"><i class="fa fa-sign-in"></i>Login</a></li>
+		<li><a href="/signup"><i class="fa fa-pencil-square-o"></i>Register</a></li>
+		</ul>
+		</>
+	) : (
+		<>
+		
+		<div className="logo"><a href="/"><p className="logo-collor">Santa Monica Tech Pioneers</p></a></div>
+		<div>
+	{/* <h5> {authUser ?` Logged in as ${authUser.username}` : ''}</h5> */}
+	</div>
+			<ul>
+				<li><a href=""><i class="fas fa-envelope"></i>Contacts</a></li>
+				<li><a href="dashboard"><i class="fas fa-cogs"></i>Back to project</a></li>
+				<li><a href="" onClick={logoutUser}><i class="fas fa-sign-out" ></i>Logout</a></li>
+				{/* <button className="header_nav_buttons"style={{background:"none"}} onClick={logoutUser}>Logout</button> */}
+			</ul>
+		</>
+	)}
+	{/* <button
+				className="nav-btn nav-close-btn"
+				onClick={showNavbar}>
+				<FaTimes />
+			</button> */}
+	</nav>
+</header> 
+
+
+
+
+
+		{/* <header>
 			
 			<h5> {authUser ?` Logged in as ${authUser.username}` : ''}</h5>
 			<h3><a href="/#">Santa Monica Tech Pioneers</a></h3>
@@ -45,8 +92,10 @@ function Navbar() {
 			</button>
 			</nav>
 		
-		</header>
-	);
+		</header> */}
+	
+		</>
+		);
 }
 
 export default Navbar;
