e1dfdb5e8d4958e50a26a980c4e9608d0f713518
Zygis911 <zygis.cer@gmail.com>
2024-05-07 22:07:21 +0300

    lets goo

---
+++ SantaMonicaTechPioneersClient/src/components/userDashboard/ProjectList/ProjectList.jsx
@@ -0,0 +1,3 @@
+import React from 'react'
+import { useProjects } from './hooks/useProject'
+
---
+++ SantaMonicaTechPioneersClient/src/components/userDashboard/ProjectList/hooks/useProject.jsx
@@ -0,0 +1,54 @@
+import React, { createContext, useCallback, useContext, useEffect, useState } from 'react'
+
+import ProjectModel from "../../../../api/projects";
+
+const ProjectContext = createContext();
+
+ export const useProjects = () => useContext(ProjectContext);
+
+ export const ProjectsProvider = ({children}) => {
+
+    const [projects, setProjects] = useState([]);
+
+    const [fetchProjects, setFetchProjects] = useState(false);
+
+    const FetchProjects = useCallback(() => {setFetchProjects(v => !v), [fetchProjects]})
+
+
+
+    const fetchProjectData = async () => {
+            const response = await ProjectModel.getMyProjects()
+            if(response.status === 200) {
+                setProjects(response.data);
+            }
+    };
+
+    const createProject = useCallback(async (project) => {
+        const response = await ProjectModel.createProject(project);
+        return [response.status, response.data];
+    });
+
+
+    const updateProject = useCallback(async(project) => {
+        const response = await ProjectModel.updateProject(project);
+        return [response.status, response.data]
+    });
+
+    const deleteProject = useCallback(async(id) => {
+        const response = await ProjectModel.deleteProject(id);
+        return [response.status, response.data]
+    });
+
+    useEffect(() => {
+        fetchProjectData();
+    }, [fetchProjects]);
+
+    return (
+        <ProjectContext.Provider value={ {projects, createProject, 
+        updateProject, deleteProject, FetchProjects, fetchProjects  }}>
+        {projects && children}
+        </ProjectContext.Provider>
+    );
+
+ };
+
