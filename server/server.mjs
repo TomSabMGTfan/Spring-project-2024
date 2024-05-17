3da89bc7f5c02b0270d9145a16cbff3966cf5954
Daniusvl <danielius.vl@gmail.com>
2024-05-18 01:53:18 +0300

    Added logger

--- server/server.mjs
+++ server/server.mjs
@@ -8,20 +8,24 @@ import { connectDB } from './db/postgresConnection.mjs';
 
 import mainRouter from './routes/mainRouter.mjs';
 
+import { LogInfo, LoggingMiddleware } from './middleware/logger.mjs';
+
 const app = express();
 
 // Server configuration
-const startServer = async () => { 
+const startServer = async () => {
 	try {
 		// Connecting to database
 		const message = await connectDB();
-		console.log(message);
+		LogInfo(message);
 
 
 		app.use(cors());
 
 		// Configuring for json body requests
 		app.use(express.json());
+
+		app.use(LoggingMiddleware);
 
 		// API routes
 		app.use('/api', mainRouter);
@@ -32,7 +36,7 @@ const startServer = async () => {
 		// Starting server
 		app.listen(port, () => {
 			// Logging
-			console.log(`Server is running and listening on port ${port}`);
+			LogInfo(`Server is running and listening on port ${port}`);
 		});
 
 	} catch (error) {
