a05021acbb65843af8ebc35fdc97e6cf68ec53ab
Daniusvl <danielius.vl@gmail.com>
2024-05-18 10:51:31 +0300

    Addition to logger

--- server/Logger/LogRouteError.mjs
+++ server/Logger/LogRouteError.mjs
@@ -1,19 +1,8 @@
 import { ExtractReqInfo, LogError } from "./Logger.mjs";
 
 export const LogRouteError = (req, res, error) => {
-
-    console.log(error);
-
-    let startTime = Date.now();
-
     res.on("finish", () => {
-        let responseTimeMS = Date.now() - startTime;
-
-        const request = ExtractReqInfo(req);
-
-        LogError("Request finished with an error.", {
-            request,
-            responseTimeMS,
+        LogError(`[${req.logger.requestId}] Request finished with an error.`, {
             error: error.stack
         });
     });
--- server/Logger/LogRouteInfo.mjs
+++ server/Logger/LogRouteInfo.mjs
@@ -1,15 +1,19 @@
 import { ExtractReqInfo, LogInfo } from "./Logger.mjs";
+import uniqid from "uniqid";
 
 export const LogRouteInfo = (req, res) => {
 
     let startTime = Date.now();
+    req.logger = {
+        requestId: uniqid()
+    };
 
     res.on("finish", () => {
         let responseTimeMS = Date.now() - startTime;
 
         const request = ExtractReqInfo(req);
 
-        LogInfo("Request finished.", {
+        LogInfo(`[${req.logger.requestId}] Request finished.`, {
             request,
             responseTimeMS
         });
